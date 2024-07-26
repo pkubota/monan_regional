82f402b4e17fad57a453bd63e5afefd58c04c465
paulo kubota <pkubota@gmail.com>
2024-07-26 17:09:34 -0300

    Add files via upload

--- run/copy_data.bash
+++ run/copy_data.bash
@@ -1,8 +1,6 @@
 #!/bin/bash -x
-path_prefix_in="/mnt/beegfs/paulo.kubota/monan_project/MPAS_Model_Regional"
-path_prefix_out="/mnt/beegfs/paulo.kubota/monan_regional"
-#${path_prefix_in}/pre/databcs/meshes/quasi_uniform/global/015_km
-#${path_prefix_out}/pre/databcs/meshes/quasi_uniform/global/015_km
+path_prefix_in="/mnt/beegfs/paulo.kubota/monan_regional"
+path_prefix_out=`cd ..;pwd`
 
 # Define a function with local variables
 
@@ -26,7 +24,6 @@ copy_mesh_variable_resolution() {
   cp -urfp ${path_in}/* ${path_out}/
 }
 
-
 # Define a function with local variables
 
 copy_mesh_WPS_GEOG() {
@@ -38,6 +35,57 @@ copy_mesh_WPS_GEOG() {
   cp -urfp ${path_in}/* ${path_out}/
 }
 
+
+# Define a function with local variables
+
+copy_mesh_datain_gfs() {
+  local path_in=${1}/pre/datain/regional/gfs/
+  local path_out=${2}/pre/datain/regional/gfs/
+
+  echo "cp -urfp: ${path_in} ${path_in}"
+  
+  cp -urfp ${path_in}/* ${path_out}/
+
+  local path_in=${1}/pre/datain/global/gfs/
+  local path_out=${2}/pre/datain/global/gfs/
+
+  echo "cp -urfp: ${path_in} ${path_in}"
+  
+  cp -urfp ${path_in}/* ${path_out}/
+
+}
+
+# Define a function with local variables
+
+copy_mesh_datain_era5() {
+  local path_in=${1}/pre/datain/regional/era5/
+  local path_out=${2}/pre/datain/regional/era5/
+
+  echo "cp -urfp: ${path_in} ${path_in}"
+  
+  cp -urfp ${path_in}/* ${path_out}/
+
+  local path_in=${1}/pre/datain/global/era5/
+  local path_out=${2}/pre/datain/global/era5/
+
+  cp -urfp ${path_in}/* ${path_out}/
+
+}
+
+# Define a function with local variables
+
+copy_mesh_exec() {
+  local path_in=${1}/pre/exec
+  local path_out=${2}/pre/exec
+
+  echo "cp -urfp: ${path_in} ${path_in}"
+  
+  cp -urfp ${path_in}/* ${path_out}/
+}
+
 copy_mesh_quasi_uniform       ${path_prefix_in}  ${path_prefix_out}
 copy_mesh_variable_resolution ${path_prefix_in}  ${path_prefix_out}
 copy_mesh_WPS_GEOG            ${path_prefix_in}  ${path_prefix_out}
+copy_mesh_datain_gfs          ${path_prefix_in}  ${path_prefix_out}
+copy_mesh_datain_era5         ${path_prefix_in}  ${path_prefix_out}
+copy_mesh_exec                ${path_prefix_in}  ${path_prefix_out}
